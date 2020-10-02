f289cf928e6e049528474818a9c3e586ce5f3116
Aarhan212 <72205689+Aarhan212@users.noreply.github.com>
2020-10-02 12:33:28 +0530

    Update Number guessing Game.cpp

--- Projects/C++/Number guessing Game.cpp
+++ Projects/C++/Number guessing Game.cpp
@@ -17,9 +17,20 @@ int main()
 		tries++;
 
 		if (guess > num)
-			cout << "Too high!\n\n";
+		{	cout << "Too high!\n\n";
+		 	if(num<50)
+				cout<<"try to guess under 50";
+		}
 		else if (guess < num)
-			cout << "Too low!\n\n";
+		{		cout << "Too low!\n\n";
+		 		if(num>50)
+					cout<<"try to guess above 50";
+		}
+		else if(guess==num-1 || guess == num+1)
+				cout<<"you are soo close";
+		else if(guess>100)
+			cout<<"are you high, enter any number between 1 and 100";
+		
 		else
 			cout << "\nCorrect! You got it in " << tries << " guesses!\n";
 	} while (guess != num);
