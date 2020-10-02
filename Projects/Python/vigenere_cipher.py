e465f810e801bb799459d6f48b11f9b9b3f0917f
David Banda <davidbandag96@gmail.com>
2020-10-02 12:18:58 -0600

    Add keyword_equal_length method

--- Projects/Python/vigenere_cipher.py
+++ Projects/Python/vigenere_cipher.py
@@ -3,3 +3,17 @@ r"""
     The encryption of the original text is done using the
     Vigenère table
 """
+
+
+def keyword_equal_length(string, keyword):
+    if len(string) == len(keyword):
+        return keyword
+
+    key_list = []
+    for i in range(len(string)):
+        idx = i % len(keyword)
+        key_list.append(keyword[idx])
+    return "".join(key_list)
+
+
+print(keyword_equal_length("DAVIDBANDA", "AARON"))
