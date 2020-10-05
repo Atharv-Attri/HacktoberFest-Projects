6efb9b08f84e0ccdb557138e7c5a43af6e1a0922
DarkWarrior703 <56077342+DarkWarrior703@users.noreply.github.com>
2020-10-05 20:58:45 +0300

    Added random search

--- Projects/C++/Computer guessing number.cpp
+++ Projects/C++/Computer guessing number.cpp
@@ -5,42 +5,93 @@ Here is an example of it : https://www.geeksforgeeks.org/binary-search/ .
 
 #include <iostream>
 #include <algorithm>
+#include <random>
 
-int main()
+void binarysearch(bool &won)
 {
-    int num;
-    system("clear");
-    std::string color = "\033[1;33m";
-    std::cout << color;
-    std::cout << "Rules:\n1.If the guess is bigger than your number, write smaller, if it is equal, write equal, otherwise bigger.\n";
     int guesses = 32;
-    bool won = false;
     int l = 1, r = 2000000;
-    while(l <= r && guesses > 0 && !won)
+    while (l <= r && guesses > 0 && !won)
     {
         int guess = l + (r - l) / 2;
         std::cout << "Computer guess is " << guess << ".\n";
         std::flush(std::cout);
         std::string user;
         std::getline(std::cin, user);
-        std::for_each(user.begin(), user.end(), [](char &c){return tolower(c);});
-        if(user == "equal")
+        std::for_each(user.begin(), user.end(), [](char &c) { return tolower(c); });
+        if (user == "equal")
         {
             std::cout << "The number was " << guess << ".\n";
             won = true;
         }
-        else if(user == "smaller")
+        else if (user == "smaller")
         {
             r = guess - 1;
         }
-        else {
+        else
+        {
             l = guess + 1;
         }
         guesses--;
-        if(!won)
-        std::cout << "The computer has " << guesses << " guesses left.\n"; 
+        if (!won)
+            std::cout << "The computer has " << guesses << " guesses left.\n";
     }
-    if(won)
+}
+
+void random(bool &won)
+{
+    int guesses = 32;
+    int l = 1, r = 2000000;
+    while (l <= r && guesses > 0 && !won)
+    {
+        std::mt19937 mt;
+        std::uniform_int_distribution<int> dist(l, r);
+        int guess = dist(mt);
+        std::cout << "Computer guess is " << guess << ".\n";
+        std::flush(std::cout);
+        std::string user;
+        std::getline(std::cin, user);
+        std::for_each(user.begin(), user.end(), [](char &c) { return tolower(c); });
+        if (user == "equal")
+        {
+            std::cout << "The number was " << guess << ".\n";
+            won = true;
+        }
+        else if (user == "smaller")
+        {
+            r = guess - 1;
+        }
+        else
+        {
+            l = guess + 1;
+        }
+        guesses--;
+        if (!won)
+            std::cout << "The computer has " << guesses << " guesses left.\n";
+    }
+}
+
+int main()
+{
+    int num;
+    system("clear");
+    std::cout << "\033[1;33m";
+    std::cout << "Rules:\n1.If the guess is bigger than your number, write smaller, if it is equal, write equal, otherwise bigger.\n";
+    std::cout << "You can choose from searching a number using binary search or by using random.\n";
+    std::cout << "1. Binary\n2. Random\n";
+    int choice;
+    bool won = false;
+    std::cin >> choice;
+    std::cin.ignore();
+    if(choice == 1)
+    {
+        binarysearch(won);
+    }
+    else 
+    {
+        random(won);
+    }
+    if (won)
     {
         std::cout << "Computer knew it.\n";
     }
@@ -48,5 +99,7 @@ int main()
     {
         std::cout << "Somehow, I was mistaken.\n";
     }
+    std::cout << "\033[0m";
+    std::cin.ignore();
     return 0;
 }
